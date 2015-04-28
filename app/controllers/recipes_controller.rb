4c13254b0063de57e89f697cc235d5cfc79961dd
jieunbug <jieunbug@gmail.com>
2015-04-27 21:30:04 -0700

    now with a working form that adds steps dynamically

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,30 @@
 class RecipesController < ApplicationController
-  def view
+  def index
+  	@recipes = Recipe.all
   end
+
+  def show
+  	@recipe = Recipe.find(params[:id])
+  	@steps = @recipe.steps
+  end
+
+  def new
+  	@recipe = Recipe.new
+
+  end
+
+  def create
+	@recipe = Recipe.new(recipe_params)
+		if @recipe.save
+			redirect_to @recipe
+		else
+			render :new
+		end
+  end
+
 end
+
+private
+  def recipe_params
+    params.require(:recipe).permit(:name, steps_attributes: [:id, :description, :active, :recipe_id, :order_in_recipe, :_destroy])
+  end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ActiveRecord::Base
 	belongs_to :meal
 	has_many :steps
-	accepts_nested_attributes_for :steps, allow_destroy: true
+	accepts_nested_attributes_for :steps, :reject_if => :all_blank, allow_destroy: true
+	validates :name, presence: true
 end
